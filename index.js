abf8746217206f8c131e38f35ada29a64bd75728
sherifhzaimia <hzaimiacherif@gmail.com>
2024-10-09 00:49:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.get('/get-token', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true, 
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
     });
 
     const page = await browser.newPage();
